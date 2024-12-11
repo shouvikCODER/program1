e0ef76f23668a57f71974a480c5c3908ea4c68f0
shouvikCODER <shouvikeditz007@gmail.com>
2024-12-11 22:58:49 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,412 @@
+namespace SpriteKind {
+    export const coin = SpriteKind.create()
+    export const flower = SpriteKind.create()
+}
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
+    game.gameOver(true)
+    game.setGameOverEffect(true, effects.dissolve)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (SAMTUL.vy == 0) {
+        SAMTUL.vy = -156
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function (sprite, location) {
+    game.gameOver(false)
+    game.setGameOverEffect(false, effects.dissolve)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.coin, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    sprites.destroy(otherSprite)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.flower, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    Bat = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    animation.runImageAnimation(
+    Bat,
+    [img`
+        . . f f f . . . . . . . . f f f 
+        . f f c c . . . . . . f c b b c 
+        f f c c . . . . . . f c b b c . 
+        f c f c . . . . . . f b c c c . 
+        f f f c c . c c . f c b b c c . 
+        f f c 3 c c 3 c c f b c b b c . 
+        f f b 3 b c 3 b c f b c c b c . 
+        . c 1 b b b 1 b c b b c c c . . 
+        . c 1 b b b 1 b b c c c c . . . 
+        c b b b b b b b b b c c . . . . 
+        c b 1 f f 1 c b b b b f . . . . 
+        f f 1 f f 1 f b b b b f c . . . 
+        f f 2 2 2 2 f b b b b f c c . . 
+        . f 2 2 2 2 b b b b c f . . . . 
+        . . f b b b b b b c f . . . . . 
+        . . . f f f f f f f . . . . . . 
+        `,img`
+        . . f f f . . . . . . . . . . . 
+        f f f c c . . . . . . . . f f f 
+        f f c c c . c c . . . f c b b c 
+        f f c 3 c c 3 c c f f b b b c . 
+        f f c 3 b c 3 b c f b b c c c . 
+        f c b b b b b b c f b c b c c . 
+        c c 1 b b b 1 b c b b c b b c . 
+        c b b b b b b b b b c c c b c . 
+        c b 1 f f 1 c b b c c c c c . . 
+        c f 1 f f 1 f b b b b f c . . . 
+        f f f f f f f b b b b f c . . . 
+        f f 2 2 2 2 f b b b b f c c . . 
+        . f 2 2 2 2 2 b b b c f . . . . 
+        . . f 2 2 2 b b b c f . . . . . 
+        . . . f f f f f f f . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . c c . c c . . . . . . . . 
+        . . f 3 c c 3 c c c . . . . . . 
+        . f c 3 b c 3 b c c c . . . . . 
+        f c b b b b b b b b f f . . . . 
+        c c 1 b b b 1 b b b f f . . . . 
+        c b b b b b b b b c f f f . . . 
+        c b 1 f f 1 c b b f f f f . . . 
+        f f 1 f f 1 f b c c b b b . . . 
+        f f f f f f f b f c c c c . . . 
+        f f 2 2 2 2 f b f b b c c c . . 
+        . f 2 2 2 2 2 b c c b b c . . . 
+        . . f 2 2 2 b f f c c b b c . . 
+        . . . f f f f f f f c c c c c . 
+        . . . . . . . . . . . . c c c c 
+        `,img`
+        . f f f . . . . . . . . f f f . 
+        f f c . . . . . . . f c b b c . 
+        f c c . . . . . . f c b b c . . 
+        c f . . . . . . . f b c c c . . 
+        c f f . . . . . f f b b c c . . 
+        f f f c c . c c f b c b b c . . 
+        f f f c c c c c f b c c b c . . 
+        . f c 3 c c 3 b c b c c c . . . 
+        . c b 3 b c 3 b b c c c c . . . 
+        c c b b b b b b b b c c . . . . 
+        c 1 1 b b b 1 1 b b b f c . . . 
+        f b b b b b b b b b b f c c . . 
+        f b c b b b c b b b b f . . . . 
+        . f 1 f f f 1 b b b c f . . . . 
+        . . f b b b b b b c f . . . . . 
+        . . . f f f f f f f . . . . . . 
+        `],
+    100,
+    true
+    )
+    Bat.setPosition(SAMTUL.x - 80, SAMTUL.y - 80)
+    Bat.follow(SAMTUL, 100)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    if (SAMTUL.y < otherSprite.y) {
+        info.changeScoreBy(3)
+    } else {
+        info.changeLifeBy(-1)
+    }
+})
+let Bat: Sprite = null
+let flower: Sprite = null
+let SAMTUL: Sprite = null
+scene.setBackgroundColor(9)
+SAMTUL = sprites.create(img`
+    ........................
+    ........................
+    ........................
+    ...........ccc..........
+    ...........cccc.........
+    .......ccc..ccccccc.....
+    .......cccccc555555cc...
+    ........ccb5555555555c..
+    .....cc..b555555555555c.
+    .....cccb55555bcc555555c
+    ......cb555555555c55d55c
+    ......b5555555555555555c
+    ...cc.b555dd5555bb1bbbc.
+    ....ccd55ddddd55bbb335c.
+    ...ccbddddddddd5bb3335c.
+    .ccccdddddddddd55b3335c.
+    cdcccdddddb55bbd553335c.
+    cddbddddddb555bb55555c..
+    cddddddddddb5555bbccc...
+    ccddddddbd55bb55cbc.....
+    .ccddddbbbdd55ccbdc.....
+    ...ccbbbcbddddccdddc....
+    .....ccccdd555dccccc....
+    ........cccccccc........
+    `, SpriteKind.Player)
+controller.moveSprite(SAMTUL)
+tiles.setCurrentTilemap(tilemap`level2`)
+info.setLife(5)
+SAMTUL.ay = 350
+controller.moveSprite(SAMTUL, 100, 0)
+scene.cameraFollowSprite(SAMTUL)
+for (let value of tiles.getTilesByType(assets.tile`myTile1`)) {
+    flower = sprites.create(img`
+        . . . . f f f f f f f . . . . . 
+        . . . f 5 5 5 5 5 5 5 f . . . . 
+        . . f 5 5 5 5 5 5 5 5 5 f . . . 
+        . f 5 5 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 4 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 4 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 4 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 4 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 5 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 5 4 5 5 5 5 5 5 5 5 f . . 
+        . . f 5 5 4 4 4 4 4 5 5 f . . . 
+        . . . f 5 5 5 5 5 5 5 f . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.coin)
+    animation.runImageAnimation(
+    flower,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . f 5 5 5 5 5 5 5 f . . . . 
+        . . f 5 4 4 4 4 4 4 4 5 f . . . 
+        . f 5 5 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 4 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 4 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 4 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 4 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 4 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 4 5 5 5 5 5 5 5 5 5 f . . 
+        . . f 5 5 4 4 4 4 4 5 5 f . . . 
+        . . . f 5 5 5 5 5 5 5 f . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . f f f f f . . . . . . 
+        . . . . f 5 5 5 5 5 f . . . . . 
+        . . . f 5 4 4 4 4 4 5 f . . . . 
+        . . f 5 5 5 5 5 5 5 5 5 f . . . 
+        . . f 5 4 5 5 5 5 5 5 5 f . . . 
+        . . f 5 4 5 5 5 5 5 5 5 f . . . 
+        . . f 5 4 5 5 5 5 5 5 5 f . . . 
+        . . f 5 4 5 5 5 5 5 5 5 f . . . 
+        . . f 5 4 5 5 5 5 5 5 5 f . . . 
+        . . f 5 4 5 5 5 5 5 5 5 f . . . 
+        . . . f 5 5 4 4 4 5 5 f . . . . 
+        . . . . f 5 5 5 5 5 f . . . . . 
+        . . . . . f f f f f . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . f 5 5 5 f . . . . . . 
+        . . . . f 5 4 4 4 5 f . . . . . 
+        . . . f 5 5 5 5 5 5 5 f . . . . 
+        . . . f 5 5 4 5 5 5 5 f . . . . 
+        . . . f 5 5 4 5 5 5 5 f . . . . 
+        . . . f 5 5 4 5 5 5 5 f . . . . 
+        . . . f 5 5 4 5 5 5 5 f . . . . 
+        . . . f 5 5 4 5 5 5 5 f . . . . 
+        . . . f 5 5 5 5 5 5 5 f . . . . 
+        . . . . f 5 5 4 5 5 f . . . . . 
+        . . . . . f 5 5 5 f . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . f 5 f . . . . . . . 
+        . . . . . f 5 4 5 f . . . . . . 
+        . . . . f 5 5 5 5 5 f . . . . . 
+        . . . . f 5 4 5 5 5 f . . . . . 
+        . . . . f 5 4 5 5 5 f . . . . . 
+        . . . . f 5 4 5 5 5 f . . . . . 
+        . . . . f 5 4 5 5 5 f . . . . . 
+        . . . . f 5 4 5 5 5 f . . . . . 
+        . . . . f 5 4 5 5 5 f . . . . . 
+        . . . . . f 5 5 5 f . . . . . . 
+        . . . . . . f 5 f . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . f 5 f . . . . . . . 
+        . . . . . . f 4 f . . . . . . . 
+        . . . . . . f 5 f . . . . . . . 
+        . . . . . . f 5 f . . . . . . . 
+        . . . . . . f 5 f . . . . . . . 
+        . . . . . . f 5 f . . . . . . . 
+        . . . . . . f 5 f . . . . . . . 
+        . . . . . . f 5 f . . . . . . . 
+        . . . . . . f 5 f . . . . . . . 
+        . . . . . . f 5 f . . . . . . . 
+        . . . . . . f 5 f . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . f 5 f . . . . . . . 
+        . . . . . f 5 4 5 f . . . . . . 
+        . . . . f 5 5 5 5 5 f . . . . . 
+        . . . . f 5 4 5 5 5 f . . . . . 
+        . . . . f 5 4 5 5 5 f . . . . . 
+        . . . . f 5 4 5 5 5 f . . . . . 
+        . . . . f 5 4 5 5 5 f . . . . . 
+        . . . . f 5 4 5 5 5 f . . . . . 
+        . . . . f 5 4 5 5 5 f . . . . . 
+        . . . . . f 5 5 5 f . . . . . . 
+        . . . . . . f 5 f . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . f 5 5 5 f . . . . . . 
+        . . . . f 5 4 4 4 5 f . . . . . 
+        . . . f 5 5 5 5 5 5 5 f . . . . 
+        . . . f 5 4 5 5 5 5 5 f . . . . 
+        . . . f 5 4 5 5 5 5 5 f . . . . 
+        . . . f 5 4 5 5 5 5 5 f . . . . 
+        . . . f 5 4 5 5 5 5 5 f . . . . 
+        . . . f 5 4 5 5 5 5 5 f . . . . 
+        . . . f 5 4 5 5 5 5 5 f . . . . 
+        . . . . f 5 5 4 5 5 f . . . . . 
+        . . . . . f 5 5 5 f . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . f f f f f . . . . . . 
+        . . . . f 5 5 5 5 5 f . . . . . 
+        . . . f 5 4 4 4 4 4 5 f . . . . 
+        . . f 5 5 5 5 5 5 5 5 5 f . . . 
+        . . f 5 4 5 5 5 5 5 5 5 f . . . 
+        . . f 5 4 5 5 5 5 5 5 5 f . . . 
+        . . f 5 4 5 5 5 5 5 5 5 f . . . 
+        . . f 5 4 5 5 5 5 5 5 5 f . . . 
+        . . f 5 4 5 5 5 5 5 5 5 f . . . 
+        . . f 5 4 5 5 5 5 5 5 5 f . . . 
+        . . . f 5 5 4 4 4 5 5 f . . . . 
+        . . . . f 5 5 5 5 5 f . . . . . 
+        . . . . . f f f f f . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . f 5 5 5 5 5 5 5 f . . . . 
+        . . f 5 4 4 4 4 4 4 4 5 f . . . 
+        . f 5 5 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 4 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 4 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 4 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 4 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 4 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 4 5 5 5 5 5 5 5 5 5 f . . 
+        . . f 5 5 4 4 4 4 4 5 5 f . . . 
+        . . . f 5 5 5 5 5 5 5 f . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `],
+    80,
+    true
+    )
+    tiles.placeOnTile(flower, value)
+    tiles.setTileAt(value, assets.tile`transparency16`)
+}
+for (let value of tiles.getTilesByType(assets.tile`myTile2`)) {
+    flower = sprites.create(img`
+        ................................
+        ................................
+        ................................
+        ................................
+        ................................
+        ................................
+        ................................
+        ................................
+        ................................
+        ................................
+        ................................
+        ...............66...............
+        .............66676.66...........
+        ..........6667757777766.........
+        ..........767775557777677.......
+        .......666777775575577777.......
+        ......66677777575555777776......
+        .......c76776777757776776.......
+        ......cc666777777777777676......
+        ......66676667777777766666......
+        ......cc76767767777667676c......
+        .......66c766677667766666c......
+        ........cc7c667667676766cc......
+        .........cc67c6767c6c66cc.......
+        ..........ccc666c6ccccc.........
+        ...........c.c66ccccccc.........
+        ...............eeee.............
+        ............eeefeefe............
+        ................ee..............
+        ................................
+        ................................
+        ................................
+        `, SpriteKind.flower)
+    tiles.placeOnTile(flower, value)
+    tiles.setTileAt(value, assets.tile`transparency16`)
+}
+game.onUpdate(function () {
+    SAMTUL.setImage(img`
+        ........................
+        ............cc..........
+        ............ccc.........
+        ........ccc.ccccccc.....
+        ........ccccc555555cc...
+        ........ccb5555555555c..
+        .....ccc.b55555ff15555c.
+        .....cccb5555555ff55555c
+        ......cb555555555555d55c
+        ....c.b555555555bb55555c
+        ....ccb555ddd5555b13bbc.
+        ....ccd55ddddd555b3335c.
+        .....cdd5ddddddd55b335c.
+        ...c.bddddb555bbbd555c..
+        ...ccdddddbb55555bccc...
+        ...ccdddddddcc555bcc....
+        .ccccdddddddddcccbcccc..
+        .cdcdddddddd55dbbbc55c..
+        .cdddddddddd555dccc5c...
+        .cbddddbbbbdd5d555cc....
+        ..cbdddbbbbbdd5555......
+        ...cccbbbbbbd5555c......
+        .....cccccccc555c.......
+        .............ccc........
+        `)
+    if (SAMTUL.vx < 0) {
+        SAMTUL.image.flipX()
+    }
+})
